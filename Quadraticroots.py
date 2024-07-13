7aa56571ec04dd77fe613ea45925381e0a9bfedf
Himanshu_Kushwaha <hk5652990@gmail.com>
2024-07-13 16:34:27 +0530

    F

--- Quadraticroots.py
+++ Quadraticroots.py
@@ -8,7 +8,7 @@ if dis>0 :
     print("Roots are distinct and real\n")
     root1 = (-b + math.sqrt(dis))/2*a
     root2 = (-b - math.sqrt(dis))/2*a
-    """HERE FOR USING THE SQRT FUNCTION YOU HAVE TO IMPORT MATH MODULE AT THE TOP OF THE CODE"""
+    """FOR USING THE SQRT FUNCTION YOU HAVE TO IMPORT MATH MODULE AT THE TOP OF THE CODE"""
 
 elif dis==0:
     print("Both Roots are equal ")
@@ -16,7 +16,7 @@ elif dis==0:
 
 else:    
     print("Roots are complex and conjugate of each other\n")
-    root_real = -b/2*a
+    root_real = -b/(2*a)
     root_img = (math.sqrt(abs(dis)))/2*a
     """HERE WE abs FUNCITON WHICH TELL THE ABSOLUTE VALUE OF THE NUMBER FROM THE NUMBER LINE
     THAT DISTANCE FROM THE 0 EXAMPLE ads(-3) = 3 , abs(3) = 3 YOU CAN THINK ITS LIKE A MODULUS"""
